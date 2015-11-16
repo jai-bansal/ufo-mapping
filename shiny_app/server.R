ef60d62f723e50e9a69176eea7ce202a7eb403d7
jai-bridg <jai@bridg.com>
2015-11-15 16:41:09 -0800

    shiny_app
    add Shiny app 'server.R' and 'ui.R' files. got a little confused w/
    commits but this should straighten everything out

--- shiny_app/server.R
+++
@@ -1,9 +0,0 @@
-# This script contains the server logic for the UFO mapping Shiny app:
-
-# Load needed packages:
-library(shiny)
-
-# Define server logic:
-shinyServer(function(input, output) {} )
-
-
