bcfc4d89817fbab2938ebe81b94e1b0c537df2d5
jai-bridg <jai@bridg.com>
2015-11-17 13:51:47 -0800

    reactive expression
    add some reactive expressions

--- server.R
+++ server.R
@@ -14,15 +14,19 @@ shinyServer(function(input, output)
   
   # Map object:
   output$plot1 = renderPlot({
-    
-    #####
+  
     # CREATE MAP:
     
-      # Get map of United States:
+      # Create reactive map of United States:
       # The specified longitude/latitude coordinates are roughly the geographic center
       # of the United States:
-      map = ggmap(get_googlemap(center = c(longitude = -96.35, latitude = 39.5), zoom = 4, 
-                                maptype = 'terrain'))
+      map = reactive({
+                      ggmap(get_googlemap(center = c(longitude = -96.35, latitude = 39.5), 
+                                          zoom = 4, maptype = 'terrain'))
+                    })
+      
+      # Create reactive version of lower date bound:
+      lower_bound = reactive({input$dates[1]})
     
       # Limit 'ufo_data' to the dates specified by user:
       # Recall the month/day for all UFO sightings is artificially set to '12/31':
@@ -41,10 +45,10 @@ shinyServer(function(input, output)
         new_upper_bound = as.Date(new_upper_bound, '%Y-%m-%d')
     
       # Limit 'ufo_data' to relevant dates:
-      rel_data = ufo_data[(date >= input$dates[1]) & (date < new_upper_bound)]
+      rel_data = ufo_data[(date >= lower_bound()) & (date < new_upper_bound)]
       
       # Add UFO sightings to map:
-      map = map + geom_point(data = rel_data, aes(x = lon, y = lat), pch = 19, size = 2, 
+      map = map() + geom_point(data = rel_data, aes(x = lon, y = lat), pch = 19, size = 2, 
                              col = 'red') + xlab('') + ylab('') + theme(axis.text.y = element_blank(), 
                                                                         axis.text.x = element_blank(), 
                                                                         axis.ticks = element_blank())
